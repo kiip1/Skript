637eda35a48b47d7890dd8dadcd1520080bf2ea1
kiip1 <25848425+kiip1@users.noreply.github.com>
2022-11-27 21:58:12 +0100

    Change creeper max fuse and no damage ticks to timespan and add tests

--- src/main/java/ch/njol/skript/expressions/ExprCreeperMaxFuseTicks.java
+++ src/main/java/ch/njol/skript/expressions/ExprCreeperMaxFuseTicks.java
@@ -18,6 +18,7 @@
  */
 package ch.njol.skript.expressions;
 
+import ch.njol.skript.util.Timespan;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.Event;
@@ -32,71 +33,60 @@ import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.util.coll.CollectionUtils;
 
-@Name("Creeper Max Fuse Ticks")
-@Description("The max fuse ticks that a creeper has.")
-@Examples("set target entity's max fuse ticks to 20 #1 second")
+@Name("Creeper Max Fuse Duration")
+@Description("The maximum fuse duration that a creeper has.")
+@Examples("set target entity's max fuse time to 1 second")
 @Since("2.5")
-public class ExprCreeperMaxFuseTicks extends SimplePropertyExpression<LivingEntity, Long> {
+public class ExprCreeperMaxFuseTicks extends SimplePropertyExpression<LivingEntity, Timespan> {
 	
 	static {
-		if(Skript.methodExists(LivingEntity.class, "getMaxFuseTicks"))
-			register(ExprCreeperMaxFuseTicks.class, Long.class, "[creeper] max[imum] fuse tick[s]", "livingentities");
+		if (Skript.methodExists(LivingEntity.class, "getMaxFuseTicks"))
+			register(ExprCreeperMaxFuseTicks.class, Timespan.class, "[creeper] max[imum] fuse (time|duration)", "livingentities");
 	}
-
+	
 	@Override
-	public Long convert(LivingEntity e) {
-		return e instanceof Creeper ? (long) ((Creeper) e).getMaxFuseTicks() : 0;
+	public Timespan convert(LivingEntity entity) {
+		return Timespan.fromTicks_i(Math.max(entity instanceof Creeper ? ((Creeper) entity).getMaxFuseTicks() : 0, 0));
 	}
 	
 	@Override
 	@Nullable
-	public Class<?>[] acceptChange(final ChangeMode mode) {
-		if (mode == ChangeMode.REMOVE_ALL)
-			return null;
-		return CollectionUtils.array(Number.class);
+	public Class<?>[] acceptChange(ChangeMode mode) {
+		return (mode != ChangeMode.REMOVE_ALL) ? CollectionUtils.array(Timespan.class) : null;
 	}
 	
 	@Override
-	public void change(final Event e, final @Nullable Object[] delta, final ChangeMode mode) {
-		int d = delta == null ? 0 : ((Number) delta[0]).intValue();
-		for (LivingEntity le : getExpr().getArray(e)) {
-			if (le instanceof Creeper) {
-				Creeper c = (Creeper) le;
-				switch (mode) {
-					case ADD:
-						int r1 = c.getMaxFuseTicks() + d;
-						if (r1 < 0) r1 = 0;
-						c.setMaxFuseTicks(r1);
-						break;
-					case SET:
-						c.setMaxFuseTicks(d);
-						break;
-					case DELETE:
-						c.setMaxFuseTicks(0);
-						break;
-					case RESET:
-						c.setMaxFuseTicks(30); //Seems to be the same for powered creepers?
-						break;
-					case REMOVE:
-						int r2 = c.getMaxFuseTicks() - d;
-						if (r2 < 0) r2 = 0;
-						c.setMaxFuseTicks(r2);
-						break;
-					case REMOVE_ALL:
-						assert false;		
-				}
-			}
+	public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
+		LivingEntity[] entities = getExpr().getArray(event);
+		int amount = delta == null ? 0 : (int) ((Timespan) delta[0]).getTicks_i();
+		switch (mode) {
+			case REMOVE:
+				amount = -amount;
+			case ADD:
+				for (LivingEntity entity : entities)
+					if (entity instanceof Creeper)
+						((Creeper) entity).setMaxFuseTicks(Math.max(((Creeper) entity).getMaxFuseTicks() + amount, 0));
+				break;
+			case DELETE:
+			case RESET:
+			case SET:
+				for (LivingEntity entity : entities)
+					if (entity instanceof Creeper)
+						((Creeper) entity).setMaxFuseTicks(Math.max(amount, 0));
+				break;
+			default:
+				assert false;
 		}
 	}
 
 	@Override
-	public Class<? extends Long> getReturnType() {
-		return Long.class;
+	public Class<? extends Timespan> getReturnType() {
+		return Timespan.class;
 	}
 
 	@Override
 	protected String getPropertyName() {
-		return "creeper max fuse ticks";
+		return "creeper max fuse time";
 	}
 	
 }
--- src/main/java/ch/njol/skript/expressions/ExprNoDamageTicks.java
+++ src/main/java/ch/njol/skript/expressions/ExprNoDamageTicks.java
@@ -18,78 +18,69 @@
  */
 package ch.njol.skript.expressions;
 
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.event.Event;
-import org.eclipse.jdt.annotation.Nullable;
-
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.util.Timespan;
 import ch.njol.util.coll.CollectionUtils;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
 
-@Name("No Damage Ticks")
-@Description("The number of ticks that an entity is invulnerable to damage for.")
+@Name("No Damage Duration")
+@Description("How much time an entity is invulnerable for.")
 @Examples({"on damage:",
-		"	set victim's invulnerability ticks to 20 #Victim will not take damage for the next second"})
+		"	set victim's invulnerability time to 1 second #Victim will not take damage for the next second"})
 @Since("2.5")
-public class ExprNoDamageTicks extends SimplePropertyExpression<LivingEntity, Long> {
+public class ExprNoDamageTicks extends SimplePropertyExpression<LivingEntity, Timespan> {
 	
 	static {
-		register(ExprNoDamageTicks.class, Long.class, "(invulnerability|no damage) tick[s]", "livingentities");
+		register(ExprNoDamageTicks.class, Timespan.class, "(invulnerability|no damage) (time|duration)", "livingentities");
 	}
 
 	@Override
-	public Long convert(LivingEntity e) {
-		return (long) e.getNoDamageTicks();
+	public Timespan convert(LivingEntity entity) {
+		return Timespan.fromTicks_i(Math.max(entity.getNoDamageTicks(), 0));
 	}
 	
 	@Override
 	@Nullable
-	public Class<?>[] acceptChange(final ChangeMode mode) {
-		if (mode == ChangeMode.REMOVE_ALL)
-			return null;
-		return CollectionUtils.array(Number.class);
+	public Class<?>[] acceptChange(ChangeMode mode) {
+		return (mode != ChangeMode.REMOVE_ALL) ? CollectionUtils.array(Timespan.class) : null;
 	}
 	
 	@Override
-	public void change(final Event e, final @Nullable Object[] delta, final ChangeMode mode) {
-		int d = delta == null ? 0 : ((Number) delta[0]).intValue();
-		for (LivingEntity le : getExpr().getArray(e)) {
-			switch (mode) {
-				case ADD:
-					int r1 = le.getNoDamageTicks() + d;
-					if (r1 < 0) r1 = 0;
-					le.setNoDamageTicks(r1);
-					break;
-				case SET:
-					le.setNoDamageTicks(d);
-					break;
-				case DELETE:
-				case RESET:
-					le.setNoDamageTicks(0);
-					break;
-				case REMOVE:
-					int r2 = le.getNoDamageTicks() - d;
-					if (r2 < 0) r2 = 0;
-					le.setNoDamageTicks(r2);
-					break;
-				case REMOVE_ALL:
-					assert false;		
-			}
+	public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
+		LivingEntity[] entities = getExpr().getArray(event);
+		int amount = delta == null ? 0 : (int) ((Timespan) delta[0]).getTicks_i();
+		switch (mode) {
+			case REMOVE:
+				amount = -amount;
+			case ADD:
+				for (LivingEntity entity : entities)
+					entity.setNoDamageTicks(Math.max(entity.getNoDamageTicks() + amount, 0));
+				break;
+			case DELETE:
+			case RESET:
+			case SET:
+				for (LivingEntity entity : entities)
+					entity.setNoDamageTicks(Math.max(amount, 0));
+				break;
+			default:
+				assert false;
 		}
 	}
 	
 	@Override
-	protected String getPropertyName() {
-		return "no damage ticks";
+	public Class<? extends Timespan> getReturnType() {
+		return Timespan.class;
 	}
 	
 	@Override
-	public Class<? extends Long> getReturnType() {
-		return Long.class;
+	protected String getPropertyName() {
+		return "no damage time";
 	}
-	
 }
